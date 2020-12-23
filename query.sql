7095be5881f924b9ead51b27b7bf8f8c9509c7b5
sokket <sokket@oceancraft.ru>
2020-12-23 20:46:34 +0300

    Fix collapsing for companies with the same name

--- query.sql
+++ query.sql
@@ -22,11 +22,14 @@ SELECT avg(CASE
 FROM vacancy;
 
 SELECT employer_name
-FROM employer
-         INNER JOIN vacancy ON vacancy.employer_id = employer.employer_id
-         INNER JOIN response ON response.vacancy_id = vacancy.vacancy_id
-GROUP BY employer_name
-ORDER BY count(response) DESC, employer_name
+FROM (
+         SELECT employer_name, count(response) AS responses_count
+         FROM employer
+                  INNER JOIN vacancy ON vacancy.employer_id = employer.employer_id
+                  INNER JOIN response ON response.vacancy_id = vacancy.vacancy_id
+         GROUP BY employer.employer_id
+     ) as enrc
+ORDER BY responses_count DESC, employer_name
 LIMIT 5;
 
 
